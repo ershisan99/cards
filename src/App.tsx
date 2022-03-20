4bae492a944333baff37f047a0cc308209ac1243
Marinade <marinadegames@gmail.com>
2022-03-20 18:40:23 +0300

    add new Route to App.tsx: * 404 * profile * reset-password * new-password * test-page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router'
+import { Route, Routes } from 'react-router-dom'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 
@@ -7,15 +7,16 @@ function App() {
     return (
         <div>
             <Routes>
-                <Route path={'/sing-in'} element={<SignIn />} />
-                <Route path={'/sing-up'} element={<SignUp />} />
+                <Route path={'/sign-in'} element={<SignIn />} />
+                <Route path={'/sign-up'} element={<SignUp />} />
 
-                {/*need fix => */}
+                {/* todo: need add component */}
+                <Route path={'/'} element={'home'} />
+                <Route path={'/profile'} element={'profile'} />
+                <Route path={'/reset-password'} element={'reset-password'} />
+                <Route path={'/new-password'} element={'new-password'} />
+                <Route path={'/test-page'} element={'test-page'} />
                 <Route path={'/*'} element={'404'} />
-                <Route path={'/profile'} element={''} />
-                <Route path={'/reset-password'} element={''} />
-                <Route path={'/new-password'} element={''} />
-                <Route path={'/test-page'} element={''} />
             </Routes>
         </div>
     )
