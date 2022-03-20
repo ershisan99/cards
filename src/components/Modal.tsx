bb248198a0e30578cfb55b44ad101211a3246c1b
Andres <zadorozhnyi99.dev@gmail.com>
2022-03-20 02:37:51 +0100

    add background to the modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -13,7 +13,7 @@ const Modal: FC<PropsType> = ({ isOpen, setIsOpen, title, children }) => {
             <Transition appear show={isOpen} as={Fragment}>
                 <Dialog
                     as="div"
-                    className="fixed inset-0 z-10 overflow-y-auto"
+                    className="fixed inset-0 z-10 overflow-y-auto bg-gradient"
                     onClose={() => {
                         setIsOpen(true)
                     }}
@@ -47,10 +47,10 @@ const Modal: FC<PropsType> = ({ isOpen, setIsOpen, title, children }) => {
                             leaveFrom="opacity-100 scale-100"
                             leaveTo="opacity-0 scale-95"
                         >
-                            <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                            <div className="my-8 inline-block w-full max-w-sm transform overflow-hidden rounded-xl border-none bg-white p-6 text-left align-middle shadow-xl transition-all">
                                 <Dialog.Title
                                     as="h3"
-                                    className="text-2xl font-semibold text-slate font-poppins text-center mb-10"
+                                    className="mb-6 text-center font-poppins text-3xl font-semibold text-secondary"
                                 >
                                     {title}
                                 </Dialog.Title>
