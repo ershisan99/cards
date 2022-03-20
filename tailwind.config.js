dcae7955b4648defa57ca33c7380e213bd097b2b
Andres <zadorozhnyi99.dev@gmail.com>
2022-03-20 02:38:37 +0100

    update color scheme, add bg img

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,25 @@ module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     darkMode: 'class',
     theme: {
-        colors: {
-            primary: '#21268f',
-            secondary: '#9a91c8',
-            'light-purple': '#d7d8ef',
-            light: '#ececf9',
-            warning: '#f1453d',
-            slate: '#2d2e46',
-        },
         extend: {
+            colors: {
+                primary: '#21268f',
+                secondary: '#9a91c8',
+                'light-purple': '#d7d8ef',
+                light: '#ececf9',
+                warning: '#f1453d',
+                slate: '#2d2e46',
+                'light-gray': '#24254a',
+                white: '#fff',
+            },
             fontFamily: {
                 sans: ['SF UI Display'],
                 sf: 'SF UI Display',
                 poppins: 'Poppins',
                 lato: 'Lato',
+            },
+            backgroundImage: {
+                gradient: 'linear-gradient(180deg, #E6D4DE 0%, #9890C7 100%)',
             },
             boxShadow: {
                 'button-primary':
