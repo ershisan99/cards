fe3cb16d6ac13b96037cd8c1862754020cef9271
Andres <zadorozhnyi99.dev@gmail.com>
2022-04-01 19:17:42 +0200

    add redux toolkit, create instance file, add provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import { Route, Routes } from 'react-router-dom'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import { store } from './state/store'
 
 function App() {
     return (
         <div>
-            <Routes>
-                <Route path={'/sign-in'} element={<SignIn />} />
-                <Route path={'/sign-up'} element={<SignUp />} />
+            <Provider store={store}>
+                <Routes>
+                    <Route path={'/sign-in'} element={<SignIn />} />
+                    <Route path={'/sign-up'} element={<SignUp />} />
 
-                {/* todo: need add component */}
-                <Route path={'/'} element={'home'} />
-                <Route path={'/profile'} element={'profile'} />
-                <Route path={'/reset-password'} element={'reset-password'} />
-                <Route path={'/new-password'} element={'new-password'} />
-                <Route path={'/test-page'} element={'test-page'} />
-                <Route path={'/*'} element={'404'} />
-            </Routes>
+                    {/* todo: need add component */}
+                    <Route path={'/'} element={'home'} />
+                    <Route path={'/profile'} element={'profile'} />
+                    <Route
+                        path={'/reset-password'}
+                        element={'reset-password'}
+                    />
+                    <Route path={'/new-password'} element={'new-password'} />
+                    <Route path={'/test-page'} element={'test-page'} />
+                    <Route path={'/*'} element={'404'} />
+                </Routes>
+            </Provider>
         </div>
     )
 }
