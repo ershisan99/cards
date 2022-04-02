458b7641b6b513b807c86ced760340fff6550607
Ivan <adamovskij00@mail.ru>
2022-04-02 12:52:13 +0300

    add upd Header component

--- src/pages/Profile/Header/Header.tsx
+++ src/pages/Profile/Header/Header.tsx
@@ -1,3 +1,24 @@
-export const Header = () => {
-    return <section>Header</section>
+import Tabs from '../../../components/Tabs'
+
+type HeaderPropsType = {
+    onTabClickHandler: () => void
 }
+
+const Header: React.FC<HeaderPropsType> = ({ onTabClickHandler }) => {
+    return (
+        <div className="h-14 w-full border border-amber-600 bg-pink-50 font-poppins">
+            <div className="mx-auto flex h-14 w-4/6 justify-between ">
+                <div className="flex grow-0 items-center  border border-red-600">
+                    <span className="w-40 text-2xl font-semibold text-gray-800">
+                        It-incubator
+                    </span>
+                </div>
+                <div className="flex grow items-center border border-blue-500">
+                    <Tabs onTabClickHandler={onTabClickHandler} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
