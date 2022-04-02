b95b559e00bbf2c2dc5a1df1a07c4d36818dd675
Ivan <adamovskij00@mail.ru>
2022-04-02 15:31:52 +0300

    add upd Content component

--- src/pages/Profile/Content.tsx
+++ src/pages/Profile/Content.tsx
@@ -1,11 +1,23 @@
-import { Header } from './Header/Header'
 import { Profile } from './Profile/Profile'
+import { useState } from 'react'
+import Header from './Header/Header'
+import Main from './Main/Main'
 
 export const Content = () => {
+    const [tabs, setTabs] = useState(true)
+    const onTabClickHandler = (value: boolean) => setTabs(value)
+
     return (
-        <div>
-            <Header />
-            <Profile />
+        <div className="h-screen w-full overflow-y-hidden bg-gradient">
+            <Header tabs={tabs} onTabClickHandler={onTabClickHandler} />
+            {tabs ? (
+                <Profile
+                    username={'Ivan Ivanov'}
+                    work={'Front-end developer'}
+                />
+            ) : (
+                <Main />
+            )}
         </div>
     )
 }
