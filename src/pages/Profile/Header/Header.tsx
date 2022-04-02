fc3a6a669cb71c15cdac1cb8cb74a304fd2419db
Ivan <adamovskij00@mail.ru>
2022-04-02 14:05:05 +0300

    add upd Header component styles

--- src/pages/Profile/Header/Header.tsx
+++ src/pages/Profile/Header/Header.tsx
@@ -1,20 +1,21 @@
-import Tabs from '../../../components/Tabs'
+import Tabs from './Tabs/Tabs'
 
 type HeaderPropsType = {
-    onTabClickHandler: () => void
+    tabs: boolean
+    onTabClickHandler: (value: boolean) => void
 }
 
-const Header: React.FC<HeaderPropsType> = ({ onTabClickHandler }) => {
+const Header: React.FC<HeaderPropsType> = ({ onTabClickHandler, tabs }) => {
     return (
-        <div className="h-14 w-full border border-amber-600 bg-pink-50 font-poppins">
+        <div className="h-14 w-full bg-pink-50 font-poppins">
             <div className="mx-auto flex h-14 w-4/6 justify-between ">
-                <div className="flex grow-0 items-center  border border-red-600">
+                <div className="flex grow-0 items-center">
                     <span className="w-40 text-2xl font-semibold text-gray-800">
                         It-incubator
                     </span>
                 </div>
-                <div className="flex grow items-center border border-blue-500">
-                    <Tabs onTabClickHandler={onTabClickHandler} />
+                <div className="flex grow items-center">
+                    <Tabs tabs={tabs} onTabClickHandler={onTabClickHandler} />
                 </div>
             </div>
         </div>
