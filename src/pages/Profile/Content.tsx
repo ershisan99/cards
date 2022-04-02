cb8e9116024ef3b7ccc26d6baea2ffa8efa4f7f4
Ivan <adamovskij00@mail.ru>
2022-04-02 10:11:00 +0300

    add Content component

---
+++ src/pages/Profile/Content.tsx
@@ -0,0 +1,11 @@
+import { Header } from './Header/Header'
+import { Profile } from './Profile/Profile'
+
+export const Content = () => {
+    return (
+        <div>
+            <Header />
+            <Profile />
+        </div>
+    )
+}
