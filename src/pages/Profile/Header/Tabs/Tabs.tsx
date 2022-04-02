ceeb818c9f3707c3c80f5d477e629bcb53f9487f
Ivan <adamovskij00@mail.ru>
2022-04-02 14:03:36 +0300

    add upd Header tabs component

---
+++ src/pages/Profile/Header/Tabs/Tabs.tsx
@@ -0,0 +1,37 @@
+import cards from '../../../../assets/cards.svg'
+import user from '../../../../assets/user.svg'
+
+type TabsPropsType = {
+    tabs: boolean
+    onTabClickHandler: (value: boolean) => void
+}
+
+const Tabs: React.FC<TabsPropsType> = ({ onTabClickHandler, tabs }) => {
+    const onMainClickHandler = () => {
+        onTabClickHandler(false)
+    }
+    const onProfileClickHandler = () => {
+        onTabClickHandler(true)
+    }
+
+    return (
+        <div className="mx-auto flex h-14 w-80">
+            <div
+                className={!tabs ? 'tab-item__active' : 'tab-item'}
+                onClick={onMainClickHandler}
+            >
+                <img className="ml-4" src={cards} alt="cards icon" />
+                <span className="mx-1.5 text-sm font-thin">Packs list</span>
+            </div>
+            <div
+                className={tabs ? 'tab-item__active' : 'tab-item'}
+                onClick={onProfileClickHandler}
+            >
+                <img className="ml-6" src={user} alt="user icon" />
+                <span className="mx-1.5 text-sm font-thin">Profile</span>
+            </div>
+        </div>
+    )
+}
+
+export default Tabs
