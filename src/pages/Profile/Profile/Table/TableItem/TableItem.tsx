83ab6a63af1ac6bd1b5a239f6dd382f80182f3a4
Ivan <adamovskij00@mail.ru>
2022-04-02 15:27:30 +0300

    add TableItem component

---
+++ src/pages/Profile/Profile/Table/TableItem/TableItem.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import Button from '../../../../../components/Button'
+
+type TabItemType = {
+    name: string
+    cards: number
+    lastUpdated: string
+    createdDay: string
+    index: number
+}
+
+const TableItem: React.FC<TabItemType> = ({
+    name,
+    cards,
+    lastUpdated,
+    createdDay,
+    index,
+}) => {
+    const tabBgStyle = {
+        backgroundColor: index % 2 !== 0 ? '#ececf9' : 'transparent',
+    }
+
+    return (
+        <>
+            <tr style={tabBgStyle}>
+                <td className="px-4 py-3">{name}</td>
+                <td className="px-4 py-3">{cards}</td>
+                <td className="px-4 py-3">{lastUpdated}</td>
+                <td className="px-4 py-3">{createdDay}</td>
+                <td className="px-4 py-3">
+                    <Button className={'ml-1 rounded px-2'} color={'warning'}>
+                        Delete
+                    </Button>
+                    <Button className={'ml-1 rounded px-2'} color={'secondary'}>
+                        Edit
+                    </Button>
+                    <Button className={'ml-1 rounded px-2'} color={'secondary'}>
+                        Learn
+                    </Button>
+                </td>
+            </tr>
+        </>
+    )
+}
+
+export default TableItem
