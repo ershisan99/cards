a991f30f108f702fa2c84aca3d671dad14097461
Andres <zadorozhnyi99.dev@gmail.com>
2022-04-03 12:39:26 +0200

    fix app name in header

--- src/pages/Profile/Header/Header.tsx
+++ src/pages/Profile/Header/Header.tsx
@@ -11,7 +11,7 @@ const Header: React.FC<HeaderPropsType> = ({ onTabClickHandler, tabs }) => {
             <div className="mx-auto flex h-14 w-4/6 justify-between ">
                 <div className="flex grow-0 items-center">
                     <span className="w-40 text-2xl font-semibold text-gray-800">
-                        It-incubator
+                        Cards
                     </span>
                 </div>
                 <div className="flex grow items-center">
