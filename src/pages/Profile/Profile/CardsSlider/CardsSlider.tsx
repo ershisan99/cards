9cb38209e032d17daa43c0f0c4ee5e8c13664f51
Ivan <adamovskij00@mail.ru>
2022-04-03 11:44:34 +0300

    add CardsSlider component

---
+++ src/pages/Profile/Profile/CardsSlider/CardsSlider.tsx
@@ -0,0 +1,45 @@
+import RangeSlider from '../../../../components/DoubleRangeSlider'
+import React, { useEffect, useState } from 'react'
+
+const MIN_VALUE: number = 0
+const MAX_VALUE: number = 100
+const STEP_VALUE: number = 1
+const ALLOW_CROSS: boolean = false
+
+const CardsSlider = () => {
+    const [value1, setValue1] = useState<number | number[]>(MIN_VALUE)
+    const [value2, setValue2] = useState<number | number[]>(MAX_VALUE)
+    const [values, setValues] = useState<any>([value1, value2])
+
+    useEffect(() => {
+        setValues([value1, value2])
+    }, [value1, value2])
+
+    const onChangeRangeSecondHandler = (value: number | number[]) => {
+        setValues(value)
+    }
+    return (
+        <div className="h-full p-6">
+            <h3 className="text-base font-semibold">Number of cards</h3>
+            <div>
+                <div className="relative mt-6 mb-4 flex justify-between text-xs">
+                    <span className="w-9 rounded bg-primary px-2 py-1 text-center text-white">
+                        {values[0]}
+                    </span>
+                    <span className="w-9 rounded bg-primary px-2 py-1 text-center text-white">
+                        {values[1]}
+                    </span>
+                </div>
+
+                <RangeSlider
+                    step={STEP_VALUE}
+                    disable={ALLOW_CROSS}
+                    value={values}
+                    onChangeRange={onChangeRangeSecondHandler}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default CardsSlider
