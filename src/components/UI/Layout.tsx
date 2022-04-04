7d144fa1342f0677f1303c1db27e6b5ed1fac250
Ivan <adamovskij00@mail.ru>
2022-04-04 11:12:05 +0300

    add layout styles

--- src/components/UI/Layout.tsx
+++ src/components/UI/Layout.tsx
@@ -5,10 +5,10 @@ const Layout: FC = ({ children }) => {
     const [tabs, setTabs] = useState(true)
     const onTabClickHandler = (value: boolean) => setTabs(value)
     return (
-        <>
+        <div className="h-full min-h-screen w-full bg-gradient">
             <Header tabs={tabs} onTabClickHandler={onTabClickHandler} />
             {children}
-        </>
+        </div>
     )
 }
 
