9bcf4892af327b07f55cd8676a9419f23e8dcb3a
Ivan <adamovskij00@mail.ru>
2022-04-04 11:26:57 +0300

    add tabs fix

--- src/pages/private/Profile/Header/Tabs/Tabs.tsx
+++ src/pages/private/Profile/Header/Tabs/Tabs.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
 import cards from '../../../../../assets/images/cards.svg'
 import user from '../../../../../assets/images/user.svg'
 
@@ -16,20 +18,23 @@ const Tabs: React.FC<TabsPropsType> = ({ onTabClickHandler, tabs }) => {
 
     return (
         <div className="mx-auto flex h-14 w-80">
-            <div
+            <Link
+                to="/main"
                 className={!tabs ? 'tab-item__active' : 'tab-item'}
                 onClick={onMainClickHandler}
             >
                 <img className="ml-4" src={cards} alt="cards icon" />
                 <span className="mx-1.5 text-sm font-thin">Packs list</span>
-            </div>
-            <div
+            </Link>
+
+            <Link
+                to="/profile"
                 className={tabs ? 'tab-item__active' : 'tab-item'}
                 onClick={onProfileClickHandler}
             >
                 <img className="ml-6" src={user} alt="user icon" />
                 <span className="mx-1.5 text-sm font-thin">Profile</span>
-            </div>
+            </Link>
         </div>
     )
 }
