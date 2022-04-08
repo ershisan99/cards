56d8afc100a2b0edf3f442b010e61cd61cf6eb35
marinadedev <marinadegames@gmail.com>
2022-04-08 20:48:46 +0300

    add exit button

--- src/pages/private/Profile/Header/Header.tsx
+++ src/pages/private/Profile/Header/Header.tsx
@@ -3,6 +3,8 @@ import { RouteNames } from '../../../../routes'
 import { userThunks } from '../../../../state/slices/UserSlice'
 import { useActions } from '../../../../utils/helpers'
 import Tabs from './Tabs/Tabs'
+import Button from '../../../../components/UI/Button'
+import React from 'react'
 
 type HeaderPropsType = {
     tabs: boolean
@@ -16,21 +18,26 @@ const Header: React.FC<HeaderPropsType> = ({ onTabClickHandler, tabs }) => {
         <div className="h-14 w-full bg-pink-50 font-poppins">
             <div className="mx-auto flex h-14 w-4/6 justify-between ">
                 <div className="flex grow-0 items-center">
-                    <span
-                        className="w-40 text-2xl font-semibold text-gray-800"
-                        onClick={() =>
-                            signOut({}).then(() => navigate(RouteNames.SIGN_IN))
-                        }
-                    >
+                    <span className="w-40 text-2xl font-semibold text-gray-800">
                         Cards
                     </span>
                 </div>
                 <div className="flex grow items-center">
                     <Tabs tabs={tabs} onTabClickHandler={onTabClickHandler} />
                 </div>
+                <div className="flex items-center">
+                    <Button
+                        className="rounded px-5 py-1 transition hover:bg-warning hover:text-white"
+                        color={'secondary'}
+                        onClick={() =>
+                            signOut({}).then(() => navigate(RouteNames.SIGN_IN))
+                        }
+                    >
+                        Exit
+                    </Button>
+                </div>
             </div>
         </div>
     )
 }
-
 export default Header
