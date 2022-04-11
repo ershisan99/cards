9a712983ec2734f6e4c32e5e9d3024eb300285ef
Ivan <adamovskij00@mail.ru>
2022-04-11 20:30:06 +0300

    add upd getCards thunk

--- src/state/slices/cardsSlice.ts
+++ src/state/slices/cardsSlice.ts
@@ -4,6 +4,14 @@ import { CardsAPI, GetCardsType } from '../../API/cardsAPI'
 export const getCards = createAsyncThunk(
     'cards/getCards',
     async (payload: GetCardsType) => {
-        return await CardsAPI.getAllCards(payload)
+        return await CardsAPI.getAllCards({
+            packName: payload?.packName,
+            min: payload?.min,
+            max: payload?.max,
+            sortPacks: payload?.sortPacks,
+            page: payload?.page,
+            pageCount: payload?.pageCount,
+            user_id: payload?.user_id,
+        })
     }
 )
