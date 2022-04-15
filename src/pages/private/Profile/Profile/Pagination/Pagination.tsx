a792b866e4e960b27e814f21ee179761f87d133e
Andres <zadorozhnyi99.dev@gmail.com>
2022-04-15 19:29:17 +0200

    fix pagination and select

--- src/pages/private/Profile/Profile/Pagination/Pagination.tsx
+++ src/pages/private/Profile/Profile/Pagination/Pagination.tsx
@@ -14,6 +14,7 @@ export const Pagination = () => {
     const { setPage, setPageCount } = useActions(cardPackActions)
     const pagesCount = Math.ceil(cardPacksTotalCount / pageCount)
     const pages: Array<number> = []
+
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i)
     }
@@ -27,11 +28,20 @@ export const Pagination = () => {
     }
     const onBackClickHandler = () => setPortionNumber(portionNumber - 1)
     const onForwardClickHandler = () => setPortionNumber(portionNumber + 1)
+    const onStartClickHandler = () => setPortionNumber(1)
+    const onEndClickHandler = () => setPortionNumber(portionCount)
 
-    let leftPortionPageNumber = (portionNumber - 1) * pageCount + 1
-    let rightPortionPageNumber = portionNumber * pageCount
+    let leftPortionPageNumber = (portionNumber - 1) * 10 + 1
+    let rightPortionPageNumber = portionNumber * 10
     return (
         <div className="mt-5 flex w-auto items-center p-0 text-sm">
+            <button
+                disabled={portionNumber <= 1}
+                className={'pagination-btn rounded transition hover:text-white'}
+                onClick={onStartClickHandler}
+            >
+                {'<<'}
+            </button>
             <button
                 disabled={portionNumber <= 1}
                 className={'pagination-btn rounded transition hover:text-white'}
@@ -70,6 +80,13 @@ export const Pagination = () => {
                 onClick={onForwardClickHandler}
             >
                 {'>'}
+            </button>
+            <button
+                disabled={portionCount <= portionNumber}
+                className="pagination-btn rounded transition hover:text-white"
+                onClick={onEndClickHandler}
+            >
+                {'>>'}
             </button>
             <div className="ml-2">
                 <span>Show</span>
