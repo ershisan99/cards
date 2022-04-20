4d051740ef1cb85c3ca357566e973847baa3c766
Ivan <adamovskij00@mail.ru>
2022-04-20 21:50:47 +0300

    add upd onChangeGrade callback

--- src/components/UI/Learn/Learn.tsx
+++ src/components/UI/Learn/Learn.tsx
@@ -2,26 +2,14 @@ import LearnModal from './LearnModal'
 import Button from '../Button'
 import { Link, useParams } from 'react-router-dom'
 import React, { useEffect, useState } from 'react'
-import { useActions, useAppSelector } from '../../../utils/helpers'
+import {
+    getRandomCard,
+    useActions,
+    useAppSelector,
+} from '../../../utils/helpers'
 import { cardsThunks, selectCards } from '../../../state/slices/cardsSlice'
 import { CardsType } from '../../../API/cardsAPI'
-import Checkbox from '../Checkbox'
-
-const getRandomCard = (cards: Array<CardsType>) => {
-    const sum = cards.reduce(
-        (acc, card) => acc + (6 - card.grade) * (6 - card.grade),
-        0
-    )
-    const rand = Math.random() * sum
-    const res = cards.reduce(
-        (acc: { sum: number; id: number }, card, i) => {
-            const newSum = acc.sum + (6 - card.grade) * (6 - card.grade)
-            return { sum: newSum, id: newSum < rand ? i : acc.id }
-        },
-        { sum: 0, id: -1 }
-    )
-    return cards[res.id + 1]
-}
+import Radio from '../Packs/Radio'
 
 const grades = [
     'Did not know',
@@ -32,13 +20,16 @@ const grades = [
 ]
 
 const Learn = () => {
-    const { cards } = useAppSelector(selectCards)
     const { id: cardsPack_id } = useParams()
-    const { getCards } = useActions(cardsThunks)
+    const { cards } = useAppSelector(selectCards)
+    const { getCards, updateGrade } = useActions(cardsThunks)
 
-    const [showAnswer, setShowAnswer] = useState<boolean>(false)
     const [card, setCard] = useState<CardsType>()
     const [first, setFirst] = useState<boolean>(true)
+    const [grade, setGrade] = useState<number>(0)
+    const [showAnswer, setShowAnswer] = useState<boolean>(false)
+
+    console.log('Grade: ' + grade)
 
     useEffect(() => {
         if (first) {
@@ -56,25 +47,57 @@ const Learn = () => {
         }
     }
 
+    const onChangeGrade = (value: string) => {
+        switch (value) {
+            case 'Did not know': {
+                setGrade(1)
+                break
+            }
+            case 'Forgot': {
+                setGrade(2)
+                break
+            }
+            case 'A lot of thought': {
+                setGrade(3)
+                break
+            }
+            case 'Сonfused': {
+                setGrade(4)
+                break
+            }
+            case 'Knew the answer': {
+                setGrade(5)
+                break
+            }
+            default:
+                setGrade(0)
+        }
+    }
+
     return (
-        <div>
+        <>
             <LearnModal isOpen={true} title={'Learn “Pack Name”'}>
                 {!showAnswer ? (
-                    <div className="my-2 mb-8 text-base">
+                    <div className="my-2 text-base">
                         <b>Question:</b> {card && card.question}
                     </div>
                 ) : (
-                    <div className="my-2 mb-8 flex flex-col text-base">
-                        <b>Question:</b> {card && card.question}
-                        <b>Answer:</b> {card && card.answer}
-                        <div className="mt-6">
+                    <div className="my-2 text-base">
+                        <div className="my-2">
+                            <b>Question:</b> {card && card.question}
+                        </div>
+                        <div className="my-2">
+                            <b>Answer:</b> {card && card.answer}
+                        </div>
+                        <div>
                             <b>Rate yourself:</b>
-                            <div className="flex flex-col">
-                                {grades.map((g, i) => (
-                                    <Checkbox alias={g} key={i}>
-                                        {g}
-                                    </Checkbox>
-                                ))}
+                            <div className="my-4">
+                                <Radio
+                                    name="radio"
+                                    options={grades}
+                                    value={grade}
+                                    onChangeOption={onChangeGrade}
+                                />
                             </div>
                         </div>
                     </div>
@@ -105,7 +128,7 @@ const Learn = () => {
                     )}
                 </div>
             </LearnModal>
-        </div>
+        </>
     )
 }
 
